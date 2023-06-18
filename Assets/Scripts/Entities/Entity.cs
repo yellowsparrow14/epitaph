93c4160d9c39054aa92de63453ffb28c795cee6d
Elizabeth Liu <esl1350@gmail.com>
2023-06-18 10:58:28 -0700

    elaborate on entity hierarchy

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    private float health;
+    [SerializeField] private float health;
     private float movementSpeed;
     private float attack;
+    private Controller ctrl;
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         //override in child classes
+        ctrl = this.gameObject.GetComponent<Controller>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         //override in child classes
     }
 
-    void Die() {
-        //override in child classes
+    protected virtual void Die() {
+        Destroy(this.gameObject);
+    }
+
+    protected virtual void TakeDamage(float dmgAmt) {
     }
 }
