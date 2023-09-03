c3cf0aa57228e81ed18df80486645936c59458b8
IanCVal94 <86568417+IanCVal94@users.noreply.github.com>
2023-09-02 20:07:03 -0400

    starting AI format for ranged attack

---
+++ Assets/Scripts/Entities/RangedEnemy.cs
@@ -0,0 +1,37 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Enemy : Entity
+// {
+//     // Start is called before the first frame update
+//     protected override void Start()
+//     {
+//         //UPDATE LATER WITH UNIQUE BEHAVIOR, IF NEEDED
+//         base.Start();
+//     }
+
+//     // Update is called once per frame
+//     protected override void Update()
+//     {
+//         //UPDATE LATER WITH UNIQUE BEHAVIOR, IF NEEDED
+//         base.Update();
+//     }
+
+//     protected override void Die() {
+//         Destroy(gameObject);
+//     }
+
+//     public override void TakeDamage(float dmgAmt) {
+//         GameObject player = GameObject.FindWithTag("Player");
+//         base.TakeDamage(dmgAmt);
+//         //Knockback(player);
+//     }
+
+//     protected override void RangeAttack() {
+//         if ()
+//         {
+            
+//         }
+//     }
+// }
