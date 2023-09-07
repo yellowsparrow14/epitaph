ae68d995cc5188fa6ebd2bb61aacd1e31553a243
IanCVal <86568417+IanCVal94@users.noreply.github.com>
2023-09-07 13:43:17 -0400

    fix commit

--- Assets/Scripts/Entities/RangedEnemy.cs
+++ Assets/Scripts/Entities/RangedEnemy.cs
@@ -1,42 +1,3 @@
-<<<<<<< Updated upstream
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class Enemy : Entity
-// {
-//     // Start is called before the first frame update
-//     protected override void Start()
-//     {
-//         //UPDATE LATER WITH UNIQUE BEHAVIOR, IF NEEDED
-//         base.Start();
-//     }
-
-//     // Update is called once per frame
-//     protected override void Update()
-//     {
-//         //UPDATE LATER WITH UNIQUE BEHAVIOR, IF NEEDED
-//         base.Update();
-//     }
-
-//     protected override void Die() {
-//         Destroy(gameObject);
-//     }
-
-//     public override void TakeDamage(float dmgAmt) {
-//         GameObject player = GameObject.FindWithTag("Player");
-//         base.TakeDamage(dmgAmt);
-//         //Knockback(player);
-//     }
-
-//     protected override void RangeAttack() {
-//         if ()
-//         {
-            
-//         }
-//     }
-// }
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -81,4 +42,3 @@ public class RangedEnemy : Entity
         //Knockback(player);
     }
 }
->>>>>>> Stashed changes
