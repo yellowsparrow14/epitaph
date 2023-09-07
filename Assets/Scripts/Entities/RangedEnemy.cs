64f414dbda151eb89fb656599451f0d7b79d14cb
IanCVal <86568417+IanCVal94@users.noreply.github.com>
2023-09-07 13:40:52 -0400

    adding new ranged goon C# file and testing

--- Assets/Scripts/Entities/RangedEnemy.cs
+++ Assets/Scripts/Entities/RangedEnemy.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // using System.Collections;
 // using System.Collections.Generic;
 // using UnityEngine;
@@ -35,3 +36,49 @@
 //         }
 //     }
 // }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemy : Entity
+{
+    GameObject target;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        //UPDATE LATER WITH UNIQUE BEHAVIOR, IF NEEDED
+        base.Start();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        //UPDATE LATER WITH UNIQUE BEHAVIOR, IF NEEDED
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+        if (dist < 1000)
+        {
+            StopEnemy(agent);
+        }
+        base.Update();
+    }
+
+    public void StopEnemy(UnityEngine.AI.NavMeshAgent agent)
+    {
+        agent.isStopped = true;
+    }
+
+    protected override void Die() {
+        Destroy(gameObject);
+    }
+
+    public override void TakeDamage(float dmgAmt) {
+        GameObject player = GameObject.FindWithTag("Player");
+        base.TakeDamage(dmgAmt);
+        //Knockback(player);
+    }
+}
+>>>>>>> Stashed changes
