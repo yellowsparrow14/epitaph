ace704fd6b91d76c7fe446c556b0e7923bc6db69
chase.obrien <chaseobrien128@gmail.com>
2023-09-08 15:10:19 -0400

    require status effect manager

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour //IEffectable
+[RequireComponent(typeof(StatusEffectManager))]
+public class Entity : MonoBehaviour 
 {
     [SerializeField] private EntityStats _entityStats;
     public EntityStats EntityStats => _entityStats;
@@ -10,6 +11,8 @@ public class Entity : MonoBehaviour //IEffectable
     private Health _health;
     public float Health => _health.health;
 
+    private StatusEffectManager _statusEffectManager;
+    public StatusEffectManager StatusEffectManager => _statusEffectManager;
 
     [SerializeField] private float knockbackDelay;
     [SerializeField] private float knockbackForce;
@@ -29,6 +32,7 @@ public class Entity : MonoBehaviour //IEffectable
 
     private void Awake() {
         _health = new(this, intialHealth);
+        _statusEffectManager = gameObject.GetComponent<StatusEffectManager>();
     }
 
     protected virtual void Start()
