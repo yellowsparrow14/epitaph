6d7856204d737c1ff29d41fb230a643b55aebbc4
Rodrigo Loza <roloza7@gmail.com>
2023-09-09 16:18:05 -0400

    Add gizmo for dagger AoE

--- Assets/Scripts/Ability System/BlinkDaggerProjectile.cs
+++ Assets/Scripts/Ability System/BlinkDaggerProjectile.cs
@@ -15,5 +15,11 @@ public class BlinkDaggerProjectile : Projectile
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
     }
 
+    // This is a hack, ideally have a debug mode for AoE (foreshadowing?)
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {}
 }
