a2b56d34430f2600d4b0614ebd6087505ee66d09
Arshiya Rahman <arshiyar2005@gmail.com>
2023-09-10 18:26:12 -0400

    Healing Terrain Changes
    - no longer uses tags to check for source -> now uses whether or not the entity has a Player script component
    - heals multiple entities of the source type

--- Assets/Scenes/Arshiya/HealingTerrain.cs
+++ Assets/Scenes/Arshiya/HealingTerrain.cs
@@ -1,39 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class HealingTerrain : MonoBehaviour
 {
-    //variable
+    //variable declarations
     [SerializeField] private string source;
     [SerializeField] private float healAmount;
     [SerializeField] private float timeInterval;
-    public GameObject entity;
+    private bool isPlayer;
+    private List<GameObject> entitiesBeingHealed = new List<GameObject>();
 
+    //called once an entity enters the healing pool
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals(source))
+        //determine if the Entity is a Player by seeing if it has the Player script
+        isPlayer = (collision.gameObject.GetComponent<Player>()!= null);
+        
+        //if it is and the source is player OR if it isn't and the source is enemy
+        if((isPlayer && source.Equals("Player")) || (!isPlayer && !source.Equals("Player")))
         {
-            entity = collision.gameObject;
+            //add the Entity to the list of entities currently in the pool
+            entitiesBeingHealed.Add(collision.gameObject);
+            Debug.Log("Length of list: " + entitiesBeingHealed.Count);
+            //heal the entity while its in the pool
             InvokeRepeating("Heal", 0f, timeInterval);
+
+        }
+        //otherwise, do nothing
+        else
+        {
+            Debug.Log("Wrong Entity!");
+        }
+        
+    }
+
+    //called when an entity leaves the pool
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        //remove the entity from the list of entities in the pool
+        entitiesBeingHealed.Remove(collision.gameObject);
+        Debug.Log("Length of list: " + entitiesBeingHealed.Count);
+        //if there are no entities
+        if(entitiesBeingHealed.Count == 0) 
+        {
+            //stop running the foreach loop
+            CancelInvoke();
+        }
+    }
+
+    //called when at least one entity is in the pool every timeInterval seconds
+    private void Heal()
+    {
+        //iterate through each entity in the pool
+        foreach(GameObject entity in entitiesBeingHealed)
+        {
+            //call the Health.Heal method on that entity
+            entity.gameObject.GetComponent<Entity>().Health.Heal(healAmount);
+            Debug.Log("Healed");
         }
 
     }
 
-    void OnTriggerExit2D(Collider2D collision)
+    //called from an external script when a Player or enemy places down a healing pool
+    //takes in a gameObject parameter that would be the gameObject that placed the healing pool down
+    public void SetSource(GameObject sourceEntity)
     {
-        CancelInvoke();
-    }
-
-    private void Heal()
-    {
-        entity.gameObject.GetComponent<Entity>().Health.Heal(healAmount);
-        Debug.Log("healed");
-    }
-
-    public void SetSource(string sourceName)
-    {
-        source = sourceName;
+        //if the source gameObject is not a player, it is assumed to be an enemy
+        source = sourceEntity.gameObject.name;
     }
    
 }
