6e56e7cafd7e1d8f2b70972122130515ebdb20c9
Elizabeth Liu <esl1350@gmail.com>
2023-09-17 23:26:36 -0400

    update splash damage projectile with player/enemy proj changes

--- Assets/Scripts/Ability System/SplashDamageProjectile.cs
+++ Assets/Scripts/Ability System/SplashDamageProjectile.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SplashDamageProjectile : Projectile
+public class SplashDamageProjectile : PlayerProjectile
 {
     [SerializeField] private float radius;
     [SerializeField] private float damage;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Collider2D[] Hit = Physics2D.OverlapCircleAll(gameObject.transform.position, radius, LayerMask.GetMask("Enemy"));
-        foreach (Collider2D collider in Hit) {
+        Debug.Log($"Hit {other.gameObject.name}");
+
+        if (other.gameObject.tag == "Enemy") {
+            base.OnTriggerEnter2D(other);
+        }
+        Collider2D[] hit = Physics2D.OverlapCircleAll(gameObject.transform.position, radius, LayerMask.GetMask("Enemy"));
+        foreach (Collider2D collider in hit) {
             Enemy enemy = collider.GetComponent<Enemy>();
             parent.GetComponent<Entity>().DealDamage(enemy, damage);
         }
