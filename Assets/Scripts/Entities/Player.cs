995ebde6796cf01d44bd630e01e1bf90eb8b1963
VictorKramer321 <143543902+VictorKramer123@users.noreply.github.com>
2023-09-23 16:54:05 -0400

    Death and respawn

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : Entity
 {
+    public override void Die() {
+        Destroy(gameObject);
+        SceneManager.LoadScene("DeathScreen");
 
-
+    }
 
 }
