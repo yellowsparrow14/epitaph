f2868ecc3e49157e715ef3f4c549e298bf21a40b
Sahil Samar <sahilsamar031@gmail.com>
2023-09-23 16:04:43 -0400

    onhit augment

--- Assets/Scripts/Augment System/OnHitAugment.cs
+++ Assets/Scripts/Augment System/OnHitAugment.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class OnHitAugment : Augment
 {
     
-    public virtual float applyAugmentDamageTaken(float damageTaken, Entity current, Entity target)
-    {
-        // used in AugmentManager to actually apply
-        return 0;
-    }
-
-    public virtual float applyAugmentDamageDealt(float damageDealt, Entity current, Entity target)
-    {
-        // used in AugmentManager to actually apply
-        return 0;
-    }
 }
