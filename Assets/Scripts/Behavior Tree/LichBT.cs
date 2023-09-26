1260db24b8e06e2f7fbd69e7203ca7fd9ebf38b6
AllisanLu <alcp2610@gmail.com>
2023-09-26 17:32:14 -0400

    Start Lich BT

--- Assets/Scripts/Behavior Tree/LichBT.cs
+++ Assets/Scripts/Behavior Tree/LichBT.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class LichBT : BehaviorTree
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Lich can spawn enemies
+    // Spells
+    //3 health crystals
 }
