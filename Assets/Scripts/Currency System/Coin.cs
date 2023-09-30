8e5f5c71dbfa4b63de515d8ad396052bb88d4ab2
chedrich3 <carsonhedrich@gmail.com>
2023-09-30 16:48:33 -0400

    Remove unused code from Coin.cs

--- Assets/Scripts/Currency System/Coin.cs
+++ Assets/Scripts/Currency System/Coin.cs
@@ -6,26 +6,12 @@ public class Coin : MonoBehaviour
 {
     // Default coin value is 10
     public int coinValue = 10;
-    // public int CurrencyVal => _value;
 
     protected Rigidbody2D rb;
-    
-    // Use if coins should expire after some time
-    // [SerializeField] protected float despawnTimer;
 
     // Start is called before the first frame update
     protected virtual void Start() {}
 
-    // Update is called once per frame
-    protected virtual void Update()
-    {
-        // if (projectileTimer > 0)
-        // {
-        //     projectileTimer -= Time.deltaTime;
-        //     return;
-        // }
-        // Destroy(this.gameObject);
-    }
 
     protected virtual void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
