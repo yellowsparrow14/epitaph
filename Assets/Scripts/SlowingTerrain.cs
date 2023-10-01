20fd799fb08e1051c55b2a15b30b224d3c147145
IanCVal94 <86568417+IanCVal94@users.noreply.github.com>
2023-09-30 22:55:58 -0400

    removed unnecessary comments

--- Assets/Scripts/SlowingTerrain.cs
+++ Assets/Scripts/SlowingTerrain.cs
@@ -23,25 +23,10 @@ public class SlowingTerrain : MonoBehaviour
     {
         //determine if the Entity is a Player by seeing if it has the Player script
         Debug.Log("New Entity being slowed");
-        ModifiableStat speed = collision.gameObject.GetComponent<Player>().EntityStats.GetStat(StatEnum.WALKSPEED);
+        ModifiableStat speed = collision.gameObject.GetComponent<Entity>().EntityStats.GetStat(StatEnum.WALKSPEED);
         //ModifiableStat speed = collision.gameObject.GetComponent<Entity>().GetStat(StatEnum.WALKSPEED);
         speed.AddModifier(modifier);
-        
-        //if it is and the source is player OR if it isn't and the source is enemy
-        // if((isPlayer && setByPlayer) || (!isPlayer && !setByPlayer))
-        // {
-        //     //add the Entity to the list of entities currently in the pool
-        //     entitiesBeingSlowed.Add(collision.gameObject);
-        //     Debug.Log("Length of list: " + entitiesBeingSlowed.Count);
-        //     //slow the entity while its in the pool
-        //     InvokeRepeating("Slow", 0f, timeInterval);
-
-        // }
-        // //otherwise, do nothing
-        // else
-        // {
         //     Debug.Log("Wrong Entity!");
-        // }
         
     }
 
@@ -49,18 +34,9 @@ public class SlowingTerrain : MonoBehaviour
     void OnTriggerExit2D(Collider2D collision)
     {
         //ModifiableStat speed = collision.gameObject.entity.EntityStats.GetStat(StatEnum.WALKSPEED);
-        ModifiableStat speed = collision.gameObject.GetComponent<Player>().EntityStats.GetStat(StatEnum.WALKSPEED);
+        ModifiableStat speed = collision.gameObject.GetComponent<Entity>().EntityStats.GetStat(StatEnum.WALKSPEED);
         speed.RemoveModifier(modifier);
         Debug.Log("Entity no longer being slowed");
-        //remove the entity from the list of entities in the pool
-        // entitiesBeingSlowed.Remove(collision.gameObject);
-        // Debug.Log("Length of list: " + entitiesBeingSlowed.Count);
-        // //if there are no entities
-        // if(entitiesBeingSlowed.Count == 0) 
-        // {
-        //     //stop running the foreach loop
-        //     CancelInvoke();
-        // }
     }
 
     //called from an external script when a Player or enemy places down a slowing pool
