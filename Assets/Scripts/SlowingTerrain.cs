f852c6faec98d01de279250e50c4ad566760732c
IanCVal94 <86568417+IanCVal94@users.noreply.github.com>
2023-10-09 00:07:28 -0400

    The slow effect is now operable with the slow terrain working

--- Assets/Scripts/SlowingTerrain.cs
+++ Assets/Scripts/SlowingTerrain.cs
@@ -6,44 +6,46 @@ using System.Threading.Tasks;
 public class SlowingTerrain : MonoBehaviour
 {
     //variable declarations
-    [SerializeField] private bool setByPlayer;
-    private bool isPlayer;
 
-    [SerializeField]
-    public float baseMoveSpeedMultiplier = 0.3f;
-    private float moveSpeedMultiplier;
-    private SpeedDecrease modifier;
+    private float timer;
+    private float timerAmt = 1.0f;
+    [SerializeField] protected List<StatusEffect> _statusEffects;
+    [SerializeField] protected List<StatusEffect> _removeStatusEffects;
+    private List<GameObject> entitiesBeingSlowed = new List<GameObject>();
 
-    //called once an entity enters the slowing pool
-    public void Init() {
-        moveSpeedMultiplier = baseMoveSpeedMultiplier;
-        modifier = new SpeedDecrease(moveSpeedMultiplier);
-    }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        //determine if the Entity is a Player by seeing if it has the Player script
         Debug.Log("New Entity being slowed");
-        ModifiableStat speed = collision.gameObject.GetComponent<Entity>().EntityStats.GetStat(StatEnum.WALKSPEED);
-        //ModifiableStat speed = collision.gameObject.GetComponent<Entity>().GetStat(StatEnum.WALKSPEED);
-        speed.AddModifier(modifier);
-        //     Debug.Log("Wrong Entity!");
+        entitiesBeingSlowed.Add(collision.gameObject);
+        var statusEffectManager = collision.GetComponent<StatusEffectManager>();
+            statusEffectManager?.ApplyEffects(_statusEffects);
         
     }
 
     //called when an entity leaves the pool
     void OnTriggerExit2D(Collider2D collision)
     {
-        //ModifiableStat speed = collision.gameObject.entity.EntityStats.GetStat(StatEnum.WALKSPEED);
-        ModifiableStat speed = collision.gameObject.GetComponent<Entity>().EntityStats.GetStat(StatEnum.WALKSPEED);
-        speed.RemoveModifier(modifier);
+        entitiesBeingSlowed.Remove(collision.gameObject);
+        var statusEffectManager = collision.GetComponent<StatusEffectManager>();
+            statusEffectManager?.ApplyEffects(_removeStatusEffects);
         Debug.Log("Entity no longer being slowed");
     }
 
+/*     void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timerAmt)
+        {
+            //iterate through each entity in the pool
+            foreach(GameObject entity in entitiesBeingSlowed)
+            {
+                var statusEffectManager = entity.GetComponent<StatusEffectManager>();
+                    statusEffectManager?.ApplyEffects(_statusEffects);
+                timer = 0;
+            }
+        }
+    } */
+
     //called from an external script when a Player or enemy places down a slowing pool
     //takes in boolean parameter that would be true if this method was called by a Player ability and false otherwise
-    public void SetSource(bool wasPlayer)
-    {
-        setByPlayer = wasPlayer;
-    }
-   
 }
