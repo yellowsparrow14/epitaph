fd5b582e4a752881edcf6a7e33aa6275b8b03769
AllisanLu <alcp2610@gmail.com>
2023-10-12 15:05:55 -0400

    Line renderers

--- Assets/Scripts/Entities/Crystal.cs
+++ Assets/Scripts/Entities/Crystal.cs
@@ -12,11 +12,27 @@ public class Crystal : Entity
     {
         lich = this.transform.parent.GetComponentInChildren<LichController>();
         lastDamaged = Time.time;
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.1f;
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.blue, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        lineRenderer.colorGradient = gradient;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //update the lines in case the lich moves :)
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        Vector3[] positions = { this.transform.position, lich.transform.position };
+        lineRenderer.SetPositions(positions);
+
         if (Time.time - lastDamaged > 2)
         {
             damaged = false;
