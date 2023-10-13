008d9b2abdbe88aad5270a2b9c256baf68d71497
Elizabeth Liu <esl1350@gmail.com>
2023-10-12 23:16:10 -0400

    restore tooltips :9

--- Assets/Scripts/Ability System/AbilityWrappers/AbilityWrapper.cs
+++ Assets/Scripts/Ability System/AbilityWrappers/AbilityWrapper.cs
@@ -5,10 +5,38 @@ using UnityEngine;
 public abstract class AbilityWrapper : ScriptableObject
 {
     [SerializeField]
+    private string wrapperName;
+    public string WrapperName {
+        get {
+            return wrapperName;
+        }
+    }
+    [SerializeField]
     private Ability activeAbility;
+
+    public Ability ActiveAbility {
+        get {
+            return activeAbility;
+        }
+    }
+
 
     [SerializeField]
     private Augment passiveAbility;
+
+    public Augment PassiveAbility {
+        get {
+            return passiveAbility;
+        }
+    }
+
+    [SerializeField]
+    private string description;
+    public string Description {
+        get {
+            return description;
+        }
+    }
 
     public Ability getActiveAbility() { 
         return activeAbility;
@@ -16,5 +44,8 @@ public abstract class AbilityWrapper : ScriptableObject
 
     public Augment getPassiveAbility() {
         return passiveAbility;
+    }
+    public string GetWrappedDescription() {
+        return description + "<br><b>Active</b>: " + activeAbility.aDescription + "<br><b>Passive</b>: " + passiveAbility.aDescription;
     }
 }
---
+++ Assets/Scripts/Ability System/AbilityWrappers/CircleDamage.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Circle Damage Ability Wrapper", menuName = "Ability Wrappers/Circle Damage")]
+public class CircleDamage : AbilityWrapper {}
